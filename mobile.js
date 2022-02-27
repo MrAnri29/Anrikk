edb8e8948facc69cf959f20012a9498ed6a3f43f
MrAnri29 <94171204+MrAnri29@users.noreply.github.com>
2022-02-27 15:45:05 +0400

    add to the top

--- mobile.js
+++ mobile.js
@@ -1,6 +1,17 @@
 let wassup = document.getElementById('welcometoesta')
+let banner1 = document.getElementById('banner1')
+let header = document.getElementById('sticky')
 
 window.addEventListener('scroll', function(){
   let value = window.scrollY;
   wassup.style.marginLeft = value + 'px';
+})
+window.addEventListener('scroll', function(){
+  let value = window.scrollY;
+  if(value > 1800){
+    header.style.opacity = '0';
+  }
+  else{
+    header.style.opacity = '1';
+  }
 })
