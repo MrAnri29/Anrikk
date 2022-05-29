30c0ce3990dfd5336aeb8fd4dea7817702111fdb
MrAnri29 <94171204+MrAnri29@users.noreply.github.com>
2022-05-29 14:50:26 +0400

    updated about.html

--- JS/aboutscript.js
+++ JS/aboutscript.js
@@ -1,14 +1,22 @@
-particlesJS.load('particles-js', '/assets/particles.json', function() {
+particlesJS.load('particles-js', '/assets/particles.json', function () {
     console.log('particles.js loaded - callback');
 });
 
 function timedRefresh(timeoutPeriod) {
-	setTimeout("location.reload(true);",timeoutPeriod);
+    setTimeout("location.reload(true);", timeoutPeriod);
 }
 
-window.onload = timedRefresh(1000*1*60*5);
+window.onload = timedRefresh(1000 * 1 * 60 * 5);
 //header/navbar
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 })
